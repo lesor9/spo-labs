126cd60698f344cb8d71956da847fa1d45a11ea2
lesor9 <lesor@protonmail.coml>
2020-12-21 19:29:34 +0300

    finally finished

--- lab_5.cpp
+++ lab_5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tchar.h>
 #include <windows.h>
 #include <time.h>
 #include <iomanip>
@@ -12,11 +13,11 @@ void thread_1()
 	srand(time(NULL)); LONG random;
 
 	for (int j = 0; j < 3; j++) {
-		HANDLE hMutex_1 = OpenMutex(SYNCHRONIZE, FALSE, "MyMutex");
+		HANDLE hMutex_1 = OpenMutex(SYNCHRONIZE, FALSE, _T("MyMutex"));
 		if (hMutex_1 == NULL)
 			cout << "Open mutex01 failed" << GetLastError() << endl;
 
-		WaitForSingleObject(hMutex_1, INFINITE); // Захват мьютекса
+		WaitForSingleObject(hMutex_1, INFINITE);
 
 		for (int i = 0; i < length; i++)
 		{
@@ -24,7 +25,7 @@ void thread_1()
 			InterlockedExchange(mass + i, random);
 		}
 
-		Sleep(100);
+		Sleep(1000);
 		cout << setw(8) << "original";
 		for (int i = 0; i < length; i++)
 		{
@@ -41,11 +42,11 @@ void thread_2()
 {
 	for (int j = 0; j < 3; j++) {
 	
-		HANDLE hMutex_2 = OpenMutex(SYNCHRONIZE, FALSE, "MyMutex");
+		HANDLE hMutex_2 = OpenMutex(SYNCHRONIZE, FALSE, _T("MyMutex"));
 		if (hMutex_2 == NULL)
 			cout << "Open mutex02 failed" << GetLastError() << endl;
 
-		WaitForSingleObject(hMutex_2, INFINITE); // Захват мьютекса
+		WaitForSingleObject(hMutex_2, INFINITE);
 
 		if (mass[0] == 0) {
 			LONG firstPositive;
@@ -57,21 +58,49 @@ void thread_2()
 			mass[0] = firstPositive;
 		}
 		
-		Sleep(100);
+		Sleep(1000);
 		cout << setw(8) << "altered";
 		for (int i = 0; i < length; i++) {
 			cout << setw(6) << mass[i];
 		}
-		cout << endl << endl;
+		cout << endl;
 
 		ReleaseMutex(hMutex_2);
 		CloseHandle(hMutex_2);
 	}
 }
 
+void thread_3()
+{
+	for (int j = 0; j < 3; j++) {
+	
+		HANDLE hMutex_3 = OpenMutex(SYNCHRONIZE, FALSE, _T("MyMutex"));
+		if (hMutex_3 == NULL)
+			cout << "Open mutex03 failed" << GetLastError() << endl;
+
+		WaitForSingleObject(hMutex_3, INFINITE);
+
+		int positiveCount;
+		for (int k = 0; k < length; k++) {
+			if (mass[k] > 0) {
+				positiveCount += 1;
+			}
+		}
+
+		Sleep(1000);
+
+		cout << "positive count: " << positiveCount << endl << endl;
+
+		positiveCount = 0;
+
+		ReleaseMutex(hMutex_3);
+		CloseHandle(hMutex_3);
+	}
+}
+
 int main()
 {
-	HANDLE hMutex = CreateMutex(NULL, FALSE, "MyMutex");
+	HANDLE hMutex = CreateMutex(NULL, FALSE, _T("MyMutex"));
 	if (hMutex == NULL)
 		cout << "Create mutex failed" << GetLastError() << endl;
 
@@ -81,10 +110,14 @@ int main()
 	HANDLE hThread_2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_2, NULL, 0, NULL);
 	if (hThread_2 == NULL) return GetLastError();
 
+	HANDLE hThread_3 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_3, NULL, 0, NULL);
+	if (hThread_3 == NULL) return GetLastError();
+
 	cin.get();
 
 	CloseHandle(hMutex);
 	CloseHandle(hThread_1);
 	CloseHandle(hThread_2);
+	CloseHandle(hThread_3);
 	return 0;
 }
--- lab_6.cpp
+++ lab_6.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tchar.h>
 #include <windows.h>
 #include <time.h>
 #include <iomanip>
@@ -8,16 +9,16 @@ HANDLE hSemaphore;
 
 
 void thread01(int** arg){
-	int* mass = arg[0];  //распаковываю указатель на массив и на его размер
+	int* mass = arg[0];
 	int length = *arg[1];
 
 	for (int i = 0; i < length; i++) {
-		WaitForSingleObject(hSemaphore, INFINITE); //беру флаг
+		WaitForSingleObject(hSemaphore, INFINITE);
 		
 		Sleep(1000); srand(time(NULL));
 		mass[i] = rand() % 501;
 		
-		ReleaseSemaphore(hSemaphore, 1, NULL); //отдаю
+		ReleaseSemaphore(hSemaphore, 1, NULL);
 	}
 }
 
@@ -27,27 +28,63 @@ void thread02(int** arg){
 	int oddCount = 0;
 
 	for (int i = 0; i < length; i++) {
-		WaitForSingleObject(hSemaphore, INFINITE); //беру
+		WaitForSingleObject(hSemaphore, INFINITE);
 		cout << "mas[" << i << "]= " << mass[i] << endl;
 
 		if (mass[i] % 2 == 1)
 			oddCount++;
 
-		ReleaseSemaphore(hSemaphore, 1, NULL); //отдаю
+		ReleaseSemaphore(hSemaphore, 1, NULL);
 	}
-	cout << "odd elements total: " << oddCount << endl;
+	cout << endl << "odd elements total: " << oddCount << endl << endl;
+	Sleep(1000);
+}
+
+void thread03(int** arg){
+	int* mass = arg[0];
+	int length = *arg[1];
+	int evenCount = 0;
+
+	for (int i = 0; i < length; i++) {
+		WaitForSingleObject(hSemaphore, INFINITE);
+
+		if (mass[i] % 2 == 0)
+			evenCount++;
+
+		ReleaseSemaphore(hSemaphore, 1, NULL);
+	}
+	
+	cout << "even elements total: " << evenCount << endl << endl;
+	Sleep(1000);
+}
+
+void thread04(int** arg){
+	int* mass = arg[0];
+	int length = *arg[1];
+	int maxOddNumber = 0;
+
+	for (int i = 0; i < length; i++) {
+		WaitForSingleObject(hSemaphore, INFINITE);
+
+		if (mass[i] % 2 == 1 && mass[i] > maxOddNumber)
+			maxOddNumber = mass[i];
+
+		ReleaseSemaphore(hSemaphore, 1, NULL);
+	}
+	
+	cout << "maxOddNumber: " << maxOddNumber << endl << endl;
 }
 
 int main()
 {
-	int n = 0;
-	cout << "array size: "; cin >> n; cout << endl;
+	int n = 3;
+	cout << "array size: " << n << endl;
 	int* mass = new int[n];
 
-	int* argument[2] = { mass, &n }; //пакую указатель на массив и на его размер в один аргумент
+	int* argument[2] = { mass, &n };
 	
 
-	hSemaphore = CreateSemaphore(NULL, 1, 1, "MySemaphore"); //начальное 1, максимум 1
+	hSemaphore = CreateSemaphore(NULL, 1, 1, _T("MySemaphore"));
 	if (hSemaphore == NULL) cout << "CREATE SEMAPHORE: failure"
 		<< GetLastError() << endl;
 
@@ -57,11 +94,19 @@ int main()
 	HANDLE hTh02 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread02, argument, 0, NULL);
 	if (hTh02 == NULL) return GetLastError();
 
+    HANDLE hTh03 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread03, argument, 0, NULL);
+	if (hTh03 == NULL) return GetLastError();
+	
+	HANDLE hTh04 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread04, argument, 0, NULL);
+	if (hTh04 == NULL) return GetLastError();
+
 	cin.ignore();
 	cin.get();
 	
 	CloseHandle(hSemaphore);
 	CloseHandle(hTh01);
 	CloseHandle(hTh02);
+    CloseHandle(hTh03);
+	CloseHandle(hTh04);
 	return 0;
 }
